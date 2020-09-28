abcbe0b57fdcf98b10c68fa6a70a32f6dfc31f40
Gustavo Coronel <gcoronelc@gmail.com>
2020-09-27 20:01:40 -0500

    Lec05

--- Lec05/SQLQuery3.sql
+++ Lec05/SQLQuery3.sql
@@ -157,5 +157,132 @@ where convert(varchar,fecingreso,103) like '__/05/2000';
 go
 
 
+-- ====================================================================
+--  <columna> [NOT] BETWEEN <limite-inferior> AND <limite-superior>
+-- ====================================================================
+
+
+select * from rh.dbo.empleado
+where sueldo between 3500 and 7500;
+go
+
+select * from rh.dbo.empleado
+order by apellido;
+go
+
+select * from rh.dbo.empleado
+where apellido between 'C'and 'M'
+order by apellido;
+go
+
+
+create table rh.dbo.demo( nombre varchar(50) );
+go
+
+
+insert into rh.dbo.demo( nombre ) 
+select nombre from rh.dbo.empleado;
+go
+
+insert into rh.dbo.demo values ('M');
+insert into rh.dbo.demo values ('F');
+go
+
+select * from rh.dbo.demo 
+order by 1;
+
+select * from rh.dbo.demo
+where nombre between 'F'and 'M'
+order by nombre;
+go
+
+select * from rh.dbo.demo
+where left(nombre,1) between 'F'and 'M'
+order by nombre;
+go
+
+select * from rh.dbo.demo
+where substring(nombre,1,1) between 'F'and 'M'
+order by nombre;
+go
+
+select * from rh.dbo.empleado
+order by fecingreso
+go
+
+select * from rh.dbo.empleado
+where fecingreso between convert(datetime,'05/04/2000',103) and convert(datetime,'05/05/2000 23:59:59',103)
+order by fecingreso
+go
+
+
+select * from rh.dbo.empleado
+where fecingreso >= convert(datetime,'05/04/2000',103) 
+and fecingreso < (convert(datetime,'05/05/2000',103) + 1)
+order by fecingreso
+go
+
+
+
+-- ====================================================================
+--  <columna> [NOT] IN (<valor1>, <valor2>, <valor3>, ....)
+--  <columna> [NOT] IN (select <columna> from <tabla> ... )
+-- ====================================================================
+
+SELECT * FROM RH.DBO.empleado where idcargo IN ('C01','C02')
+GO
+
+SELECT * FROM RH.DBO.empleado 
+where idcargo IN ( select idcargo from rh.dbo.cargo where nombre like 'Gerente%' );
+GO
+
+
+/*
+Problema 13.	
+Desarrollar una sentencia SELECT que permita consultar los empleados 
+que su nombre finaliza con la letra "O". 
+Base de datos RH.
+*/
+
+/* Claudia */
+select * from rh.dbo.empleado where nombre like '%o';
+go
+
+
+/*
+Problema 14.
+Desarrollar una sentencia SELECT que permita consultar los empleados 
+que su apellido tiene en la segunda posición la letra "A" ó "O". 
+Base de datos RH.
+*/
+
+/* Huaman */
+select * from rh.dbo.empleado
+where apellido like '_A%' or apellido like '_O%';
+go
+
+
+/*
+Problema 15.
+Desarrollar una sentencia SELECT que permita averiguar que empleados 
+tienen un sueldo mayor de 3,000.0 y menor de 10,000.0. 
+Base de datos RH.
+*/
+
+/* Soberon */
+SELECT * FROM DBO.empleado
+WHERE sueldo BETWEEN 3000.0 AND 10000.0
+GO
+
+
+/*
+Problema 16.
+Desarrollar una sentencia SELECT que permita averiguar quiénes son los empleados 
+de los departamentos de contabilidad y ventas. 
+Base de datos RH.
+*/
+
+
+
 
 
